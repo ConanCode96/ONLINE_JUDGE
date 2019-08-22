2e4cc61f2e69c9235d2ab8ced4293c61387273c1
Hossam <conancode96@gmail.com>
2019-08-22 12:02:37 +0200

    added contest generators

---
+++ Codeforces/gen_contest.sh
@@ -0,0 +1,11 @@
+contest_name=$1
+mkdir $contest_name
+cd $contest_name
+
+cp ../../tmp.cpp A.cpp
+cp ../../tmp.cpp B.cpp
+cp ../../tmp.cpp C.cpp
+cp ../../tmp.cpp D.cpp
+cp ../../tmp.cpp E.cpp
+
+echo "Done! GoodLuck Sir \")))" 
