39f0734e70dcee8786fa145e9f55422ecfb7f46c
Hossam <conancode96@gmail.com>
2019-11-02 17:54:50 +0200

    added tmp template

---
+++ tmp.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define X first
+#define Y second
+#define all(x) x.begin(), x.end()
+
+const int MX = (int)1e5 + 10;
+const int mod = (int)1e9 + 7;
+
+void add_self(int &x, int y){x += y; if(x >= mod) x -= mod;}
+void sub_self(int &x, int y){x -= y; if(x < 0) x += mod;}
+void mul(int &x, int y){x = x * 1LL * y % mod;}
+
+
+int main(int argc, char* argv[]){
+
+    cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+
+    
+    return 0;
+}
