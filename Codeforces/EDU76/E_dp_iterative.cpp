24db1ce8d40eb83d0772ddfa03e3b1c06893b6a0
Conan <conancode96@gmail.com>
2019-11-14 01:39:06 +0200

    added iterative DP solution for E

---
+++ Codeforces/EDU76/E_dp_iterative.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define X first
+#define Y second
+#define all(x) x.begin(), x.end()
+
+const int MX = (int)2e5 + 10;
+const int mod = (int)1e9 + 7;
+
+void add_self(int &x, int y){x += y; if(x >= mod) x -= mod;}
+void sub_self(int &x, int y){x -= y; if(x < 0) x += mod;}
+void mul(int &x, int y){x = x * 1LL * y % mod;}
+
+int located[MX];
+int dp[MX][3];
+
+int calc(int problem, int idx, int n){
+
+    if (idx == 3)
+        return (int)1e9;
+
+    if(problem == n + 1) return 0;
+
+    int& res = dp[problem][idx];
+    if(res != -1) return res;
+
+    res = calc(problem, idx + 1, n); // consider it at the next idx
+
+    res = min(res, calc(problem + 1, idx, n) + (located[problem] != idx));
+
+    return res;
+}
+
+int main(){
+
+    cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    
+    int a, b, c;
+    cin >> a >> b >> c;
+    vector<int> v1(a), v2(b), v3(c);
+    for(auto &x : v1) cin >> x, located[x] = 0;
+    for(auto &x : v2) cin >> x, located[x] = 1;
+    for(auto &x : v3) cin >> x, located[x] = 2;
+
+    // memset(dp, 0xff, sizeof dp);
+    // cout << calc(1, 0, a + b + c) << endl;
+
+    memset(dp, 0x7f, sizeof dp);
+
+    dp[0][0] = dp[0][1] = dp[0][2] = 0; 
+
+    for(int idx = 0; idx < 3; ++idx){
+        for(int i = 1; i <= a + b + c; ++i){
+            if(idx) dp[i][idx] = dp[i][idx - 1];
+            dp[i][idx] = min(dp[i][idx], dp[i - 1][idx] + (located[i] != idx));
+        }
+    }
+
+    cout << dp[a + b + c][2] << endl;
+
+    return 0;
+}
