c9245e0d8ec75ae00e97cc8280ffb2436c3da2ed
Hossam <conancode96@gmail.com>
2019-11-15 05:27:11 +0200

    added contest generation script

---
+++ gen_contest.sh
@@ -0,0 +1,15 @@
+contest_name=$1
+mkdir $contest_name
+cd $contest_name
+
+cp ../../tmp.cpp A.cpp
+cp ../../tmp.cpp B.cpp
+cp ../../tmp.cpp C.cpp
+cp ../../tmp.cpp D.cpp
+cp ../../tmp.cpp E.cpp
+
+cp ../../stress.sh
+cp ../../testlib.h
+cp ../../gen.cpp
+
+echo "Done! GoodLuck Sir \")))" 
