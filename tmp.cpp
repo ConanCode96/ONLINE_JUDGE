8b614cdce0753be28005e4a0bb4d56b03d92a24c
Hossam <conancode96@gmail.com>
2019-11-17 02:35:26 +0200

    edited tab size

--- tmp.cpp
+++ tmp.cpp
@@ -11,7 +11,7 @@ using namespace std;
 #define ris return * this
 #define dor > debug & operator <<
 #define eni(x) sim > typename \
-  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+	enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
 sim > struct rge { c b, e; };
 sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
 sim > auto dud(c* x) -> decltype(cerr << *x, 0);
@@ -22,13 +22,13 @@ struct debug {
 eni(!=) cerr << boolalpha << i; ris; }
 eni(==) ris << range(begin(i), end(i)); }
 sim, class b dor(pair < b, c > d) {
-  ris << "(" << d.first << ", " << d.second << ")";
+	ris << "(" << d.first << ", " << d.second << ")";
 }
 sim dor(rge<c> d) {
-  *this << "[";
-  for (auto it = d.b; it != d.e; ++it)
-    *this << ", " + 2 * (it == d.b) << *it;
-  ris << "]";
+	*this << "[";
+	for (auto it = d.b; it != d.e; ++it)
+		*this << ", " + 2 * (it == d.b) << *it;
+	ris << "]";
 }
 #else
 sim dor(const c&) { ris; }
@@ -46,10 +46,9 @@ void mul(int &x, int y){x = x * 1LL * y % mod;}
 
 int main(){
 
-    cin.tie(0); cout.tie(0);
-    ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+	ios_base::sync_with_stdio(0);
 
-    
-    
-    return 0;
+	
+	return 0;
 }
