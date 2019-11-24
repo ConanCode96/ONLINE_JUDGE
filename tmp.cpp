ef6a56457e75e360456e6bc6a980e8f68125ec61
Conan <conancode96@gmail.com>
2019-11-24 13:36:49 +0200

    added ordered set headers to the template

--- tmp.cpp
+++ tmp.cpp
@@ -1,11 +1,15 @@
 #include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
 
 using namespace std;
-
+using namespace __gnu_pbds; 
+  
 #define ll long long
 #define X first
 #define Y second
 #define all(x) x.begin(), x.end()
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> 
 
 #define sim template < class c
 #define ris return * this
